c37678d3426463d1cf1ee8a0a8978e9e4d70db5c
Benjamin Martin <benjaminmartin@Mac-mini-de-Benjamin.local>
2024-04-25 09:16:03 +0200

    ok

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Fragment, useContext, useEffect, useRef } from 'react';
+import React, { FC, useContext, useRef } from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import styles from './styles.module.css';
 
