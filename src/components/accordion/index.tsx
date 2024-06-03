29c0ebc2c815bea79cbc59772b27e7dd4f1614b2
Benjamin Martin <benjammartin@gmail.com>
2024-06-03 12:12:15 +0200

    ok

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -37,7 +37,7 @@ const Accordion: React.FC<{
       return;
     }
     setActiveRows(items);
-  }, [rows, collaps]);
+  }, [items, collaps]);
 
   const onAddNewItem = React.useCallback(() => {
     dispatch({
