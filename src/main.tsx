367e77491826be8c53fdc4ad2cdb8ebf738469f7
Benjamin Martin <benjammartin@gmail.com>
2024-06-03 12:03:51 +0200

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -3,41 +3,10 @@ import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './reset.css';
 import AppContextProvider from './contexts/app-provider.tsx';
-import SolutionOneTabsPageBuilder from './demos/solution-one-tabs/index.tsx';
-import SolutionThreePageBuilder from './demos/solution-three-collaps-group/index.tsx';
+
 import SolutionTwoPageBuilder from './demos/solution-two/index.tsx';
-import Preview from './demos/preview/index.tsx';
-import SliceMachine from './demos/slicemachine/index.tsx';
 
 const router = createBrowserRouter([
-  {
-    path: '/preview',
-    element: (
-      <AppContextProvider>
-        <Preview />
-      </AppContextProvider>
-    ),
-  },
-  {
-    path: '/sm',
-    element: <SliceMachine />,
-  },
-  {
-    path: '/solution-one',
-    element: (
-      <AppContextProvider>
-        <SolutionOneTabsPageBuilder />
-      </AppContextProvider>
-    ),
-  },
-  {
-    path: '/solution-three',
-    element: (
-      <AppContextProvider>
-        <SolutionThreePageBuilder />
-      </AppContextProvider>
-    ),
-  },
   {
     path: '/',
     element: (
